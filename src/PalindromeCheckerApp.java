94232f6df29a06b4200dd281d54c9d9917510ea5
raviprakashreddyramireddy <raviprakashreddyramireddy@gmail.com>
2026-02-27 09:44:09 +0530

    UC6:QUEUE AND STACK BASED PALINDROME

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
-    // Method to check palindrome using Stack
+    // Method to check palindrome using Queue and Stack
     public static boolean isPalindrome(String input) {
 
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
-        // Push all characters into the stack
+        // Enqueue and Push characters
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            queue.add(ch);      // Enqueue (FIFO)
+            stack.push(ch);     // Push (LIFO)
         }
 
-        // Pop characters and compare with original string
-        for (int i = 0; i < input.length(); i++) {
-            char poppedChar = stack.pop();
-            if (input.charAt(i) != poppedChar) {
+        // Compare Dequeue and Pop results
+        while (!queue.isEmpty()) {
+
+            char fromQueue = queue.remove();   // Dequeue
+            char fromStack = stack.pop();      // Pop
+
+            if (fromQueue != fromStack) {
                 return false;
             }
         }
@@ -28,7 +36,7 @@ public class PalindromeCheckerApp {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== UC5: Stack-Based Palindrome Checker ===");
+        System.out.println("=== UC6: Queue + Stack Based Palindrome Check ===");
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
